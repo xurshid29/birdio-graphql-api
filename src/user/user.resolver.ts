9d7635e404d8d201e956609634456cb49979625a
xurshid29 <xurshid29@gmail.com>
2022-10-01 19:24:59 +0500

    feat: add profile query

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,8 +1,11 @@
-import { Args, Mutation, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { User } from "./user.model";
 import { UserInput } from "./dto/user.input";
 import { UserService } from "./user.service";
-import { UsePipes, ValidationPipe } from "@nestjs/common";
+import { UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
+import { GraphqlJwtAuthGuard } from "../auth/graphql-jwt-auth.guard";
+import { CurrentUser } from "../auth/graphql-current-user.decorator";
+import { Project } from "../project/project.model";
 
 @Resolver(() => User)
 export class UserResolver {
@@ -12,6 +15,15 @@ export class UserResolver {
   @UsePipes(new ValidationPipe({ transform: true }))
   async register(@Args("input") input: UserInput): Promise<User> {
     const user = await this.userService.register(input);
+    return {
+      id: user.id,
+      email: user.email,
+    };
+  }
+
+  @UseGuards(GraphqlJwtAuthGuard)
+  @Query(() => User)
+  async profile(@CurrentUser() user: User): Promise<User> {
     return {
       id: user.id,
       email: user.email,
