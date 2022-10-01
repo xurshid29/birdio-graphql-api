25da24c3364e1fdb636dffc67ff87f9491f3345d
xurshid29 <xurshid29@gmail.com>
2022-10-01 14:17:21 +0500

    fix: add validation constraint to the create-project input

--- src/project/dto/create-project.input.ts
+++ src/project/dto/create-project.input.ts
@@ -1,7 +1,9 @@
 import { Field, InputType } from "@nestjs/graphql";
+import { IsNotEmpty } from "class-validator";
 
 @InputType()
 export class CreateProjectInput {
   @Field()
+  @IsNotEmpty()
   name: string;
 }
